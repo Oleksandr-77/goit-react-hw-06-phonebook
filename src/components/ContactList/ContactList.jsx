d214fb8df708bd654e36d36676fd493982153b41
Oleksandr Kokhno <kohnoan@gmail.com>
2023-06-13 01:29:05 +0300

    commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,35 +1,33 @@
-import PropTypes from 'prop-types';
 import css from './contactList.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contacts/contacts-slice';
+import { getFilteredContacts } from 'redux/contacts/contacts-selectors';
 
-const ContactList = ({ contacts, onDeleteContact }) => {
-    return (
-      <ul className={css.contactList}>
-        {contacts.map(contact => (
-          <li className={css.contactItem} key={contact.id}>
-            <span>{contact.name}: </span>
-            <span>{contact.number}</span>
-            <button
-              className={css.deleteButton}
-              type="button"
-              onClick={() => onDeleteContact(contact.id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getFilteredContacts);
+
+  const onDeleteContact = id => {
+    dispatch(deleteContact(id));
   };
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }).isRequired
-  ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
+  return (
+    <ul className={css.contactList}>
+      {contacts.map(contact => (
+        <li className={css.contactItem} key={contact.id}>
+          <span>{contact.name}: </span>
+          <span>{contact.number}</span>
+          <button
+            className={css.deleteButton}
+            type="button"
+            onClick={() => onDeleteContact(contact.id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ContactList;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,36 @@
-import PropTypes from 'prop-types';
+import React from 'react';
 import css from './filter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from 'redux/filter/filter-slice';
+import { getFilter } from 'redux/filter/filter-selectors';
 
-const Filter = props => {
+const Filter = () => {
+  const onSetFilter = payload => {
+    dispatch(setFilter(payload));
+  };
+
   const updateFilter = event => {
-    props.onFilter(event.target.value);
+    onSetFilter(event.target.value);
   };
 
-    return (
-      <div className={css.filter}>
-        <label className={css.label} htmlFor="filter">
-          Find contacts by name
-        </label>
-        <input
-          className={css.filterInput}
-          type="text"
-          name="filter"
-          onChange={updateFilter}
-          value={props.filter}
-        />
-      </div>
-    );
-  };
+  const dispatch = useDispatch();
 
-Filter.propTypes = {
-  filter: PropTypes.string.isRequired,
-  onFilter: PropTypes.func.isRequired,
+  const filter = useSelector(getFilter);
+
+  return (
+    <div className={css.filter}>
+      <label className={css.label} htmlFor="filter">
+        Find contacts by name
+      </label>
+      <input
+        className={css.filterInput}
+        type="text"
+        name="filter"
+        onChange={updateFilter}
+        value={filter}
+      />
+    </div>
+  );
 };
 
 export default Filter;
